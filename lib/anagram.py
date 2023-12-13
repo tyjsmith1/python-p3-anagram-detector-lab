852d4001d299f872535f6d79d72906b99aa31ecc
tylersmith <tyjsmith1@gmail.com>
2023-12-13 16:45:02 -0600

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        
+    def match(self, word_list):
+        anagrams = []
+        for word in word_list:
+            if sorted(word) == sorted(self.word):
+                anagrams.append(word)
+        return anagrams
